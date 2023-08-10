f29f781ebb311b08aa8445a7f939f87581b0853d
kimiihvo <kimiihvo@gmail.com>
2023-08-10 23:35:11 +0900

    level1: 내적

---
+++ src/level1/내적/Solution.java
@@ -0,0 +1,12 @@
+package level1.³»Àû;
+
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+}
