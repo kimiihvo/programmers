d3535c34d5ce152acad865a7c6d31520bc7f4ade
kimiihvo <kimiihvo@gmail.com>
2023-08-12 23:28:53 +0900

    level1: 부족한 금액 계산하기

---
+++ src/level1/부족한금액계산하기/Solution.java
@@ -0,0 +1,15 @@
+package level1.부족한금액계산하기;
+
+class Solution {
+    public long solution(int price, int money, int count) {
+        long sum = 0;
+
+        for (int i = 1; i <= count; i++) {
+            sum += price * i;
+        }
+
+        long answer = sum - money;
+
+        return answer > 0 ? answer : 0;
+    }
+}
