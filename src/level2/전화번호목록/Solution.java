a342cbcdb5f8d33f79b0a58a95e80a4f4a9cdf61
kimiihvo <kimiihvo@gmail.com>
2023-09-06 21:36:57 +0900

    level2: 전화번호목록

---
+++ src/level2/전화번호목록/Solution.java
@@ -0,0 +1,21 @@
+package level2.전화번호목록;
+
+import java.util.Arrays;
+
+class Solution {
+    public static boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
+            }
+
+        }
+
+        return answer;
+    }
+}
