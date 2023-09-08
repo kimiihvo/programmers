464ec3295a748162068265ea89c72a75273e057d
kimiihvo <kimiihvo@gmail.com>
2023-09-09 00:43:05 +0900

    level1: 두개 뽑아서 더하기

---
+++ src/level1/두개뽑아서더하기/Solution.java
@@ -0,0 +1,22 @@
+package level1.두개뽑아서더하기;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+
+        Set<Integer> set = new TreeSet<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        int[] answer = set.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+        return answer;
+    }
+}
