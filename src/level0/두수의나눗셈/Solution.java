e16e60ef2699130d6d5bf3951785943752a56523
kimiihvo <kimiihvo@gmail.com>
2023-09-28 00:00:21 +0900

    두 수의 나눗셈

---
+++ src/level0/두수의나눗셈/Solution.java
@@ -0,0 +1,8 @@
+package level0.µÎ¼öÀÇ³ª´°¼À;
+
+class Solution {
+    public int solution(int num1, int num2) {
+        double answer = Double.valueOf(num1) / Double.valueOf(num2) * 1000;
+        return (int) answer;
+    }
+}
