4d823b5cfcd2bb26287d933cf3216c6912a8de67
kimiihvo <kimiihvo@gmail.com>
2023-12-11 00:46:50 +0900

    level0: 배열 뒤집기

--- src/level0/배열뒤집기/Solution.java
+++ src/level0/배열뒤집기/Solution.java
@@ -1,7 +1,9 @@
+package level0.배열뒤집기;
+
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length];
-        for (int i = num_list.length - 1, j=0; i >= 0; i--,j++ ){
+        for (int i = num_list.length - 1, j = 0; i >= 0; i--, j++) {
             answer[j] = num_list[i];
         }
         return answer;
