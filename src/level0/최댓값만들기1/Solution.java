2e84c3a6a13541a54f5d0b929c57e4b03dd9681c
kimiihvo <kimiihvo@gmail.com>
2024-01-13 16:54:55 +0900

    level0: 최댓값 만들기1

---
+++ src/level0/최댓값만들기1/Solution.java
@@ -0,0 +1,10 @@
+package level0.최댓값만들기1;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
+    }
+}
