1d66c98728fc6fbbfb564e7fb2957bc97268bf88
kimiihvo <kimiihvo@gmail.com>
2024-01-21 21:20:36 +0900

    level0: 숨어있는 숫자의 덧셈1

---
+++ src/level0/숨어있는숫자의덧셈1/Solution.java
@@ -0,0 +1,15 @@
+package level0.¼û¾îÀÖ´Â¼ıÀÚÀÇµ¡¼À1;
+
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+
+        for (int i = 0; i < my_string.length(); i++) {
+            if (Character.isDigit(my_string.charAt(i))) {
+                answer += Character.getNumericValue(my_string.charAt(i));
+            }
+        }
+        return answer;
+    }
+
+}
