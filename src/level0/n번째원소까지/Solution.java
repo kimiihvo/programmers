6d8b4a87de577ba547f37512d714330f0200883a
kimiihvo <kimiihvo@gmail.com>
2024-02-23 23:33:03 +0900

    level0: n 번째 원소까지

--- src/level0/n번째원소까지/Solution.java
+++ src/level0/n번째원소까지/Solution.java
@@ -1,11 +1,13 @@
+package level0.n번째원소까지;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
         int[] answer = new int[n];
-        
-        for (int i = 0; i < n; i++){
+
+        for (int i = 0; i < n; i++) {
             answer[i] = num_list[i];
         }
-        
+
         return answer;
     }
 }
