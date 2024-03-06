14c3cb61ada7b86bb5502405abbd25154f813133
kimiihvo <kimiihvo@gmail.com>
2024-03-06 21:48:12 +0900

    level0: 글자 이어 붙여 문자열 만들기

---
+++ src/level0/글자이어붙여문자열만들기/Solution.java
@@ -0,0 +1,13 @@
+package level0.글자이어붙여문자열만들기;
+
+class Solution {
+    public String solution(String my_string, int[] index_list) {
+        String answer = "";
+
+        for (int i = 0; i < index_list.length; i++) {
+            answer += my_string.charAt(index_list[i]);
+        }
+
+        return answer;
+    }
+}
