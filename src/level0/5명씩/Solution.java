ca5e94b7a0ca092b93c3fb51c99c98af20252cd7
kimiihvo <kimiihvo@gmail.com>
2024-03-23 00:33:10 +0900

    level0: 5명씩

---
+++ src/level0/5명씩/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String[] solution(String[] names) {
+        int len = (names.length % 5 == 0) ? names.length / 5 : names.length / 5 + 1;
+        String[] answer = new String[len];
+        
+        for (int i = 0, j = 0; i < names.length; i += 5, j++ ){
+            answer[j] = names[i];
+        }
+        
+        return answer;
+    }
+} 
